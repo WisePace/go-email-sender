305825025f3fc56d5d4e8f73cb0b405cbbca3b8a
vervikar1995 <vervikar@hotmail.com>
2024-11-12 17:17:12 +0200

    feat: Implement main function to send emails to valid recipients

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"pace-sender/utils"
+)
+
+func main() {
+	// Get the list of valid email addresses
+	validEmails, err := utils.GetValidEmails()
+	if err != nil {
+		log.Fatalf("Error validating emails: %v", err)
+	}
+
+	// Send emails only to valid email addresses
+	err = utils.SendEmailsToValidRecipients(validEmails)
+	if err != nil {
+		log.Fatalf("Error sending emails: %v", err)
+	}
+}
